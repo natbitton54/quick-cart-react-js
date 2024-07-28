dacbee9477699834bbbbe2cfb692189c8fbb975d
natbitton54 <84545346+natbitton54@users.noreply.github.com>
2024-07-28 15:50:17 -0400

    Update server.js

--- my-server/server.js
+++ my-server/server.js
@@ -240,7 +240,7 @@ app.get('/api/lowestResale', async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
 })
